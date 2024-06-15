6c119600a79f8c7d1ed3947bc01318d5d1945342
정회민 <wony1102@naver.com>
2024-06-15 23:30:04 +0900

    refactor(App-login): axios 요청 중 필요없는 부분 코드에서 제거

--- nts-admin-app/src/modules/apis/apis.login.ts
+++ nts-admin-app/src/modules/apis/apis.login.ts
@@ -1,10 +1,9 @@
 import type { RequestLoginType, ResponseLoginType } from '@/types/types.login';
 import axiosInstance, { getAxiosError } from '@/modules/apis/index';
-import type { AxiosResponse } from 'axios';
 
 export const login = async <T = ResponseLoginType, R = RequestLoginType>(data: R) => {
 	try {
-		const response = await axiosInstance.post<T, AxiosResponse<T>, R>('/manager/login', data);
+		const response = await axiosInstance.post<T>('/manager/login', data);
 		return response.data;
 	} catch (e) {
 		throw getAxiosError(e);
